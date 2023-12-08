1fe5dcd125836de33dfbdb9b0616ce8dd6bb8827
David Montaño Tamayo <cuatro04sr@gmail.com>
2023-12-08 18:29:41 -0500

    feat: add network and kubernetes configuration

---
+++ terraform/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = var.state_resource_group_name
+    storage_account_name = var.state_storage_account_name
+    container_name       = "perf-cal"
+    key                  = "cal-terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_kubernetes_cluster" "cluster" {
+  depends_on = [
+    module.aks,
+    module.rg
+  ]
+  name                = module.aks.aks_name
+  resource_group_name = module.rg.rg_name
+}
+
+provider "azurerm" {
+  features {}
+}
