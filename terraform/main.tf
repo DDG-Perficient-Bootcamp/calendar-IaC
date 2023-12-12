1107454a1cb3e9f2f970283f17aa66015edf2923
David Montaño Tamayo <cuatro04sr@gmail.com>
2023-12-11 21:58:47 -0500

    feat: added sonar and metrics

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,20 +16,12 @@ module "snet" {
   vnet_name  = module.vnet.vnet_name
 }
 
-module "pubip" {
-  depends_on  = [module.rg]
-  source      = "./modules/azurerm/public_ip"
-  rg_name     = module.rg.rg_name
-  rg_location = module.rg.rg_location
-}
-
 module "ni" {
   depends_on  = [module.rg, module.snet, module.pubip]
   source      = "./modules/azurerm/network_interface"
   rg_name     = module.rg.rg_name
   rg_location = module.rg.rg_location
   sn_id       = module.snet.snet_id
-  pubip_id    = module.pubip.pubip_id
 }
 
 module "nsg" {
@@ -58,4 +50,31 @@ module "aks" {
   source      = "./modules/azurerm/kubernetes_cluster"
   rg_name     = module.rg.rg_name
   rg_location = module.rg.rg_location
+}
+module "sa" {
+  depends_on  = [module.rg]
+  source      = "./modules/azurerm/storage_account"
+  rg_name     = module.rg.rg_name
+  rg_location = module.rg.rg_location
+}
+
+module "ss" {
+  depends_on = [module.sa]
+  source     = "./modules/azurerm/storage_share"
+  sa_name    = module.sa.sa_name
+}
+
+module "cg" {
+  depends_on  = [module.rg, module.sa, module.ss]
+  source      = "./modules/azurerm/container_group"
+  rg_name     = module.rg.rg_name
+  rg_location = module.rg.rg_location
+  ss_name     = module.ss.ss_name
+  sa_name     = module.sa.sa_name
+  sa_key      = module.sa.sa_key
+}
+
+module "helm" {
+  depends_on = [module.aks]
+  source     = "./modules/helm/release"
 }
